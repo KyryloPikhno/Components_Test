5eba3f5281b255236d2e891e827f4beb0919ddc8
KyryloPikhno <Mr.Good@i.ua>
2023-04-24 13:07:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useCallbackOne } from "use-memo-one";
 import {useEffect, useRef, useState} from "react";
 import {stringTimer} from "./stringTimer";
+import moment from "moment/moment";
 
+const HourMinuteSecondsFormat = "HH:mm:ss";
 
 function App() {
     const countdown = useRef(null);
@@ -27,11 +29,34 @@ function App() {
         };
     }, [timeToSendCode, timerInterval]);
 
-    {/*setTimeToSendCode(moment(newConfirmationCodeAfter).format());*/}
+
+    const currentDate = new Date();
+
+    // Получить текущее время в миллисекундах
+    const currentTime = currentDate.getTime();
+
+    // Добавить 5 минут в миллисекундах (1 минута = 60 000 миллисекунд)
+    const futureTime = currentTime + (5 * 60 * 1000);
+
+    // Создать новый объект Date, используя будущее время в миллисекундах
+    const futureDate = new Date(futureTime);
+
+    // Сформировать строку в нужном формате
+    const formattedDate = futureDate.toISOString();
+
+    // Вывести результат в консоль
+    useEffect(() => {
+        // console.log(timeToSendCode);
+        // setTimeToSendCode(moment(formattedDate).format())
+        if (timeToSendCode) {
+            const timeToSendCodeMoment = moment(timeToSendCode);
+            console.log(moment.duration(timeToSendCodeMoment.format(HourMinuteSecondsFormat)));
+        }
+    }, [timeToSendCode]);
     return (
         <div>
 
-
+        <button onClick={()=> setTimeToSendCode(moment(formattedDate).format())}>Get timeToSendCode</button>
         </div>
     );
 }
