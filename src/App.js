18556383ad6d39e91ca8ea46ecb407dc8f94bfd8
KyryloPikhno <Mr.Good@i.ua>
2023-05-20 16:18:52 +0300

    add useLayoutEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import {ItemRef} from "./components/useRef/ItemRef";
+import {useLayoutEffect, useState} from "react";
 
 function App() {
+    const [users, setUsers] = useState([]);
 
-    return <ItemRef/>;
+    useLayoutEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/users');
+                const data = await response.json();
+                setUsers(data);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    return (
+        <div>
+            <h1>User List</h1>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
